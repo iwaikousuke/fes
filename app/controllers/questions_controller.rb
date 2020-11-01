c6fe73455e33c9333379a085b2467aa8e4eb1d31
iwaikousuke <71627635+iwaikousuke@users.noreply.github.com>
2020-11-02 06:32:47 +0900

    質問機能　実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,29 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
+    @questions = Question.includes(:user).order('created_at DESC')
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(question_params)
+    if @question.valid?
+      @question.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :text).merge(user_id: current_user.id)
   end
 end
---
+++ app/models/question.rb
@@ -0,0 +1,5 @@
+class Question < ApplicationRecord
+
+  validates :title, :text, presence: true
+  belongs_to :user
+end
