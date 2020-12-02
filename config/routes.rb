56eeed2acdc4facd36c212d4d0c200a14ed0ed7b
iwaikousuke <71627635+iwaikousuke@users.noreply.github.com>
2020-12-03 00:08:47 +0900

    いいね　編集[質問]

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :questions do
     resources :answers
-    resources :likes, only: [:create, :destroy]
   end
+  resources :questions do
+    resources :likes, only: [:create, :destroy]
+    end
   resources :users, only: :show
 end
