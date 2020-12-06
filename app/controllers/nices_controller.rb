72162892270ed1dc5b65d35db4735c9a5b118a78
iwaikousuke <71627635+iwaikousuke@users.noreply.github.com>
2020-12-06 15:22:44 +0900

    編集ver 2

--- app/controllers/nices_controller.rb
+++ app/controllers/nices_controller.rb
@@ -1,6 +1,6 @@
 class NicesController < ApplicationController
   def create
-    @nices = current_user.nices.create(answer_id: params[:answer_id], user_id: current_user.id)
+    @nices = current_user.nices.create(answer_id: params[:answer_id])
     redirect_back(fallback_location: root_path)
   end
 
